63fa23ca7c2b5beb3a18aeca4d06766f29907431
apple <apple@apples-MacBook-Pro.local>
2020-04-06 19:40:03 +0300

    init project

---
+++ src/main/java/net/javaci/javaciBank/JavaciBankApplication.java
@@ -0,0 +1,13 @@
+package net.javaci.javaciBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaciBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaciBankApplication.class, args);
+	}
+
+}
